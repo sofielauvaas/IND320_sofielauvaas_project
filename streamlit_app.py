f6cb095aae05555baa0250aa8819cbe5a4b4b9aa
sofielauvaas <sofie.seland.sveindal@nmbu.no>
2025-10-03 12:56:21 +0200

    Added home page picture, changed config colors

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,6 @@ Welcome to my IND320 project app.
 This app reads a local CSV file (`open-meteo-subset.csv`) and visualizes it.
 Use the sidebar to navigate to different pages.
 """)
+
+# Display the image at the bottom
+st.image("home_page.png", use_container_width=True)
