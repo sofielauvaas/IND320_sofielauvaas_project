1c0c4d9665f390979a0c5fc319be8b550cd128fd
sofielauvaas <sofie.seland.sveindal@nmbu.no>
2025-10-22 11:22:16 +0200

    updated part 1 to some of peer comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ st.set_page_config(page_title="IND320 - Sofie Lauvaas Project", layout="wide")
 st.title("IND320 — Home Page")
 st.markdown("""
 Welcome to my IND320 project app.  
-This app reads a local CSV file (`open-meteo-subset.csv`) and visualizes it.
 Use the sidebar to navigate to different pages.
 """)
 
